160897ff6cd39ac8113ab290ecffed0ff05d8aed
Patrick Reichelt <patrick.reichelt@capgemini.com>
2017-06-23 17:16:19 +0200

    Extend simulator to store and publish Routes of Vehicles (from Group 8).

--- src/main/java/com/capgemini/hackathon/device/simulation/bo/Vehicle.java
+++ src/main/java/com/capgemini/hackathon/device/simulation/bo/Vehicle.java
@@ -4,6 +4,7 @@ import org.joda.time.DateTime;
 
 import com.capgemini.hackathon.device.simulation.DeviceClientConfig;
 import com.capgemini.hackathon.device.simulation.model.Location;
+import com.capgemini.hackathon.device.simulation.model.Route;
 import com.capgemini.hackathon.device.simulation.model.VehicleLocation;
 import com.capgemini.hackathon.device.simulation.routing.MapCoordinatePoint;
 import com.capgemini.hackathon.device.simulation.routing.RouteCalculator;
@@ -16,7 +17,7 @@ public abstract class Vehicle extends Simulation {
 
 	// How close the vehicles reach their destination
 	private static final double DIST_LAT_LONG = 0.00005;
-	private static final double DEFAULT_SPEED = 0.0001;
+	private static final double DEFAULT_SPEED = 0.0003;
 	// The steps driving the vehicles per iteration
 	private final static double SPEED = Vehicle.getSPEED();
 
@@ -24,6 +25,8 @@ public abstract class Vehicle extends Simulation {
 	private Location currentLocation;
 	// destination Location
 	private Location destination;
+	// route Information
+	private Route route;
 
 	public Vehicle(DeviceClientConfig deviceClientConfig, Location location, Object id) {
 		super(deviceClientConfig, id);
@@ -48,6 +51,15 @@ public abstract class Vehicle extends Simulation {
 			e.printStackTrace();
 		}
 
+	}
+
+	protected void publishRoute() {
+		try {
+			JsonObject JsonRoute = route.asJson();
+			getDeviceClient().publishEvent(Route.EVENT, JsonRoute);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Location getCurrentLocation() {
@@ -91,6 +103,13 @@ public abstract class Vehicle extends Simulation {
 			return;
 		}
 		
+		route = Route.fromGHRes(this.getId().toString(), response);
+		try {
+			this.publishRoute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		int i = 0;
 		long millis = DateTime.now().getMillis();
 		long previousMillis = millis;
